11178c2c4a46fa42709ca9d8ffa37b95edfb17a1
klibin <km.putnam2@gmail.com>
2022-03-10 12:48:19 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,23 @@
+#Personal Portfolio Project#
+
+This Codecademy project was designed to bring together CSS,  HTML, and Vanilla Javascript into one page. 
+
+The design and content were entirely up to me, but the requirements included:
+
+
++ Build a website using HTML, CSS
++Add at least one interactive feature using JavaScript
++Use Git version control
++Use the command line and file navigation
++Develop locally on your computer
++Deploy your site to the web using GitHub Pages
+
+
+I used FlexBox and was pleased to see how quickly the layout came together. I honestly had the most problem figuring
+out how to style the page - it seems a little more high pressure when it's supposed to be your own page that shows off
+your coding stuff. I learned about some new CSS properties and learned more about CSS animations. 
+
+This was my first time incorporating JavaScript into a page - I was pretty excited about it, though look forward to learning
+more about how to streamline the code. I was having issues creating multiple Event Listeners on the pink mouse icon, so I 
+ended up creating to separate icons (alternating their opacity so that only one appears on the screen at any given time).
+
---
+++ resources/javascript/index.js
@@ -0,0 +1,82 @@
+
+//let eventTarget = document.getElementById('targetElement');
+ 
+//eventTarget.addEventListener('click', function() {
+
+
+
+let mainColor = document.querySelector('.main-container');
+
+let mouseClick = document.querySelector('.fa-computer-mouse');
+
+let image = document.querySelector('.kphoto');
+
+let text = document.querySelector('.intro');
+
+let navColor = document.querySelector('.cchange');
+
+let toggle = document.querySelector('.toggle');
+
+let normal = document.querySelector('.normal');
+
+let textDisplay1 = document.querySelector('.display1');
+
+let textDisplay2 = document.querySelector('.display2');
+
+let textDisplay3 = document.querySelector('.display3');
+
+let about = document.querySelector('.about');
+
+let figure = document.querySelector('.main-container figure');
+  
+
+function mainChange() {
+  
+  mainColor.style.background = 'linear-gradient(1deg, black, #f71609, #f98904, #f5fd00, #05ec07, #02fbe1, hotpink)';
+  mainColor.style.backgroundSize = '1200% 1200%';
+  mainColor.style.animation = " change 10s ease infinite";
+  image.src= "resources/images/squarenose.jpg";
+  image.style.maxWidth = "100%";
+  text.style.backgroundImage = "url('resources/images/thisvar.png')";
+  textDisplay1.style.display = "none";
+  textDisplay2.style.display = "none";
+  textDisplay3.style.display = "none";
+  text.style.width = "70%";
+  text.style.minHeight = "300px";
+  text.style.backgroundColor = "transparent";
+  text.style.boxShadow = "none";
+  navColor.style.backgroundColor = "black";
+  toggle.style.opacity = "1";
+  normal.style.display = "none";
+  figure.style.paddingRight = "2.35em";
+  
+
+}
+
+mouseClick.addEventListener('click', mainChange);
+
+function undo() {
+
+  mainColor.style.background =  "linear-gradient(212deg, #2f274a, #6f1051, #049ab1)";
+  mainColor.style.backgroundSize = '1200% 1200%';
+  mainColor.style.animation = "change 20s ease infinite";
+  image.src = "resources/images/square.jpg";
+  textDisplay1.style.display = "";
+  textDisplay2.style.display = "";
+  textDisplay3.style.display = "";
+  text.style.backgroundImage = "url()";
+  text.style.boxShadow = "";
+  text.style.backgroundColor = "";
+  text.style.minHeight = "";
+  text.style.maxWidth = "";
+  text.style.marginLeft = "";
+  navColor.style.backgroundColor = "";
+  toggle.style.opacity = "";
+  normal.style.display = "";
+  figure.style.paddingRight = "";
+
+}
+
+toggle.addEventListener('click', undo);
+
+
